efadbe967592310df1f64177d9533ee2d5f8f3ac
Vinayak <vinayakagrawal506@proton.me>
2026-05-06 23:59:01 +0530

    run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-rm Build/binaries/*
 x86_64-elf-gcc -ffreestanding -m32 -g -c SRC/kernel.c -o Build/kernel.o
 x86_64-elf-gcc -ffreestanding -m32 -g -c SRC/Hardware\ Files/ports.c -o Build/ports.o
 x86_64-elf-gcc -ffreestanding -m32 -g -c SRC/Hardware\ Files/keyboard.c -o Build/keyboard.o
@@ -8,7 +7,3 @@ nasm SRC/Boot-Level\ Files/boot.asm -f bin -o Build/binaries/boot.bin
 cat Build/binaries/boot.bin Build/binaries/kernel.bin > Build/binaries/rawOSfile.bin
 cat Build/binaries/rawOSfile.bin Build/zero.bin > Build/binaries/withZeroes.bin
 qemu-system-x86_64 -drive format=raw,file="Build/binaries/rawOSfile.bin",index=0,if=floppy -m 128M
-
-
-# nasm milestones/Bootloader1.asm -f bin -o milestones/Bootloader.bin
-# qemu-system-x86_64 milestones/Bootloader.bin
